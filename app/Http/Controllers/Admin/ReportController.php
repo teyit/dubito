377fa8f9c55d134e41001c2e253b7292c82ccec0
Öner Çiller <onerciller@gmail.com>
2017-02-19 15:40:42 +0300

    s3 config was added

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Model\Cases;
 use App\Model\Topic;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use League\Flysystem\Filesystem;
 
 class ReportController extends Controller
 {
+
+    public function index(){
+
+    }
+
     public function create(){
       $topics = Topic::latest()->get();
       $cases = Cases::all();
