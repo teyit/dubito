c5f58c3016b281f2314c6ffe6358d4de883ff462
serafettin <serafettin@serafettin.net>
2017-03-03 10:41:37 +0300

    Login page

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ Route::get("/",function(){
 });
 
 
-Route::group(['namespace' => 'Admin'], function () {
+Route::group(['namespace' => 'Admin', 'middleware' => 'auth'], function () {
     
     Route::resource("categories",'CategoryController');
     Route::resource("topics","TopicController");
     Route::resource("reports","ReportController");
-    Route::put('/report_files/{id}/status',['as'=>'report_files.status','uses'=>'ReportController@status']);
     Route::resource("cases","CaseController");
     Route::resource("tags","TagController");
+    Route::put('/report_files/{id}/status',['as'=>'report_files.status','uses'=>'ReportController@status']);
 });
 
 
