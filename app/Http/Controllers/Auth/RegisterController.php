3dd110d562340742d39dada759b7dd2239e9d4e1
serafettin <serafettin@serafettin.net>
2017-03-03 10:45:35 +0300

    Disable registeration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/reports';
 
     /**
      * Create a new controller instance.
@@ -62,6 +62,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        return redirect('login'); //TODO reenable registering
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
