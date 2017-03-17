20da3d409036d9556dc61a3c2eab2a615f6fdfea
Öner Çiller <onerciller@gmail.com>
2017-03-17 22:08:41 +0300

    fixed

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -15,7 +15,7 @@ class MessageController extends Controller
 
         $senders = Message::selectRaw('messages.*,count(id) as count')->groupBy('sender_id','recipient_id')->get();
 
-        $notReadMessages = Message::selectRaw('messages.*,count(id) as count')->groupBy('sender_id','recipient_id')->get()->count();
+        $notReadMessages = Message::selectRaw('messages.*,count(id) as count')->where('is_read',0)->groupBy('sender_id','recipient_id')->get()->count();
 
 
         $messages = Message::where('sender_id','1672136149469483')->get();
