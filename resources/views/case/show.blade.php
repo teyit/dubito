b13b19be2d4f40b476a361631d8ac938f8b482fa
serafettin <serafettin@serafettin.net>
2017-06-18 18:32:36 +0300

    deleted user fix

--- resources/views/case/show.blade.php
+++ resources/views/case/show.blade.php
@@ -327,7 +327,7 @@
 
                                 @foreach($case->evidences as $evidence)
                                     <li class="latest">
-                                        <span class="timeline-autor">{{$evidence->user->name}}</span> - <span
+                                        <span class="timeline-autor">{{$evidence->user->name or "Deleted User"}}</span> - <span
                                                 style="color:#8c8c8c;padding-left:5px;">{{\Carbon\Carbon::parse($evidence->created_at)->format("d.m.Y")}}</span>
                                         <div class="user-timeline-description">{{$evidence->text or ''}}</div>
                                         <div class="gallery-container evidence-gallery-container">
