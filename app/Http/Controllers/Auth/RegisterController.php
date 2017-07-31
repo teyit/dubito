02c0b0fece38c69281147cb0d886011bb7e0451a
serafettin <serafettin@serafettin.net>
2017-07-31 15:30:58 +0300

    closes #36

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/reports';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
