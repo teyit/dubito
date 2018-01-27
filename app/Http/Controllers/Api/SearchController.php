129943e028afa2271b6dde64ebfc398a50484722
Öner Çiller <onerciller@gmail.com>
2018-01-27 15:59:31 +0300

    search by tags with cases

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -13,20 +13,35 @@ use App\Http\Controllers\Controller;
 class SearchController extends Controller
 {
     public function search(){
+
         $results = [];
         if(request()->has('term')){
 
             $term = request()->get('term');
-            $cases = Cases::where('title', 'LIKE', '%'.$term.'%')->get()->toArray();
 
-            foreach($cases as $case){
-                $result["id"] = $case['id'];
-                $result["text"] = $case['title'];
-                $result["category"] = "Case";
-                $result["url"] = "/cases/".$case["id"];
-                $results[] = $result;
+//           $cases = Cases::where('title', 'LIKE', '%'.$term.'%')->get()->toArray();
+
+            $cases = Cases::join('case_tags', 'case_tags.case_id','=','cases.id')
+            ->select("cases.title as case_title", "cases.id as case_id")
+            ->join('tags','tags.id','=','case_tags.tag_id')
+            ->where('cases.title', 'LIKE', '%' . $term . '%')
+            ->orWhere('tags.title','LIKE', '%' . $term . '%')
+            ->orderBy('cases.created_at', 'desc')
+            ->groupBy('cases.id')
+            ->with('tags')
+            ->get()->toArray();
+
+            if(! empty($cases)){
+                foreach($cases as $case){
+                    $result["id"] = $case['case_id'];
+                    $result["text"] = $case['case_title'];
+                    $result["category"] = "Case";
+                    $result["url"] = "/cases/".$case["case_id"];
+                    $results[] = $result;
+                }
+                return $results;
             }
-           return $results;
+
         }
 
         return [];
