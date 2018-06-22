e59edd602169190cfce207ed6915ab100ea5858a
Can Çitoğlu <scan.citoglu@tedu.edu.tr>
2018-06-22 19:54:01 +0300

    add missing class to report-case selector

--- resources/views/report/partials/_form.blade.php
+++ resources/views/report/partials/_form.blade.php
@@ -37,7 +37,7 @@
             <label class="col-sm-3 control-label">Case</label>
             <div class="col-sm-6">
                 <div class="input-group xs-mb-15">
-                    <select name="case_id" id="rep-cases" required class="report-case-select2 report-cases">
+                    <select name="case_id" id="rep-cases" required class="report-case-select2 form-control report-cases">
                         @foreach($cases as $case)
                             @if($case->id == $report->case_id)
                                 <option value="{{$case->id}}" selected>{{$case->title}}</option>
