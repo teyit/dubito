8eef4b65b96bb7822a90b82f20f83af70c1a2836
Can Çitoğlu <scan.citoglu@tedu.edu.tr>
2018-07-23 13:23:28 +0300

    remove admin check for evidence controller

--- app/Http/Controllers/Admin/EvidenceController.php
+++ app/Http/Controllers/Admin/EvidenceController.php
@@ -13,7 +13,7 @@ class EvidenceController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('role:Admin');
+   //     $this->middleware('role:Admin');
     }
 
 
